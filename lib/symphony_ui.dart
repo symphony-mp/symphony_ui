d97d202e9c0b00c636e6d40845d91c9227162e9a
João Sereia <joao.lo.sereia@gmail.com>
2025-10-10 13:57:01 -0300

    chore(analysis_options): add very_good_analysis

--- lib/symphony_ui.dart
+++ lib/symphony_ui.dart
@@ -1,5 +1,3 @@
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+/// An open source music player that contains the main local and offline
+/// features.
+library;
